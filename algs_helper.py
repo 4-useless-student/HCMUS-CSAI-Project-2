663edb32a86ad2d6207fed90dc067eb56f451d96
ngocquan1 <quanphanpq147@gmail.com>
2025-12-08 14:18:39 +0000

    create project

---
+++ algs_helper.py
@@ -0,0 +1,51 @@
+#Import thư viện
+
+
+# Định nghĩa Base class cho các giải thuật
+class BaseSolver:
+    def __init__(self, input_file):
+        pass
+
+    def parse_input(self):
+        pass
+
+    def solve(self):
+        pass
+
+    def format_solution(self):
+        pass
+
+    def save_output(self, output_file):
+        pass
+    
+    def run(self):
+        pass
+
+    def visualize():
+        pass
+
+# Định nghĩa chi tiết từng giải thuật
+
+class CNFSolver(BaseSolver):
+    def __init__():
+        pass
+    def solve():
+        pass
+
+class AStarSolver(BaseSolver):
+    def __init__():
+        pass
+    def solve():
+        pass
+
+class BacktrackingSolver(BaseSolver):
+    def __init__():
+        pass
+    def solve():
+        pass
+
+class BruteForceSolver(BaseSolver):
+    def __init__():
+        pass
+    def solve():
+        pass
